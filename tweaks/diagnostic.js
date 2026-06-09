ebaae3c919c570273752a45636042c9ea1f3ccc9
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-09 16:53:40 +0200

    Update diagnostic.js

--- tweaks/diagnostic.js
+++ tweaks/diagnostic.js
@@ -7,9 +7,14 @@ var first = r_msg2(windows, "objectAtIndex:", 0);
 
 var subs = r_msg2(first, "subviews");
 
-if (subs != 0)
-    log("Got subviews");
-
 var count = r_msg2(subs, "count");
 
 log("Subviews = " + count);
+
+if (count > 0) {
+
+    var child = r_msg2(subs, "objectAtIndex:", 0);
+
+    if (child != 0)
+        log("Got child");
+}
