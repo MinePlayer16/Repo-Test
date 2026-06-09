5b42b770b7771cbb319fbe93fc85db71c741e85a
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-09 16:51:52 +0200

    Add logging for subview count in diagnostic.js

--- tweaks/diagnostic.js
+++ tweaks/diagnostic.js
@@ -1,1 +1,15 @@
-r_msg2_main(searchPill, "setHidden:", 1);
+log("Start");
+
+var app = r_msg2(r_class("UIApplication"), "sharedApplication");
+var windows = r_msg2(app, "windows");
+
+var first = r_msg2(windows, "objectAtIndex:", 0);
+
+var subs = r_msg2(first, "subviews");
+
+if (subs != 0)
+    log("Got subviews");
+
+var count = r_msg2(subs, "count");
+
+log("Subviews = " + count);
