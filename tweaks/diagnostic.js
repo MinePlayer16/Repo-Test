ee2b1bc156654d0e26d31b7295c5002f95c89976
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-09 16:47:00 +0200

    Update diagnostic.js

--- tweaks/diagnostic.js
+++ tweaks/diagnostic.js
@@ -1,14 +1,1 @@
-log("Searching windows");
-
-var app = r_msg2(r_class("UIApplication"), "sharedApplication");
-var windows = r_msg2(app, "windows");
-
-var count = r_msg2(windows, "count");
-
-for (var i = 0; i < count; i++) {
-
-    var w = r_msg2(windows, "objectAtIndex:", i);
-
-    if (w != 0)
-        log("Window " + i + " = " + w);
-}
+r_msg2_main(searchPill, "setHidden:", 1);
