e5d07c00fe5524cd0a165fad77acc233e59ced84
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-09 16:08:44 +0200

    Update diagnostic.js

--- tweaks/diagnostic.js
+++ tweaks/diagnostic.js
@@ -1,17 +1,9 @@
 log("Start");
 
-var appCls = r_class("UIApplication");
-
-var app = r_msg2(appCls, "sharedApplication");
-
-if (app != 0)
-    log("Got app");
-
+var app = r_msg2(r_class("UIApplication"), "sharedApplication");
 var windows = r_msg2(app, "windows");
 
-if (windows != 0)
-    log("Got windows");
+var first = r_msg2(windows, "objectAtIndex:", 0);
 
-var count = r_msg2(windows, "count");
-
-log("Count = " + count);
+if (first != 0)
+    log("Got first window");
