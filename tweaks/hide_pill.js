26d3f860e368a8ef17513f82ac3df507fcc196d6
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-14 14:28:10 +0200

    Update hide_pill.js

--- tweaks/hide_pill.js
+++ tweaks/hide_pill.js
@@ -16,24 +16,4 @@ if (pillClass != 0) {
     log("ERROR: SBHSearchPillView class not found.");
 }
 
-// Approach B: Direct active instance manipulation (Instant hide)
-var sbControllerClass = r_class("SBIconController");
-if (sbControllerClass != 0) {
-    var ctrl = r_msg2(sbControllerClass, "sharedInstance");
-    var mgr = r_msg2(ctrl, "iconManager");
-    var rootFolderCtrl = r_msg2(mgr, "rootFolderController");
-    
-    if (rootFolderCtrl != 0) {
-        var rootFolderView = r_msg2(rootFolderCtrl, "view");
-        if (rootFolderView != 0) {
-            var searchPill = r_msg2(rootFolderView, "searchPillView");
-            if (searchPill != 0) {
-                r_msg2_main(searchPill, "setHidden:", 1);
-                r_msg2_main(searchPill, "setAlpha:", 0.0);
-                log("SUCCESS: Active search pill instance hidden instantly!");
-            } else {
-                log("WARNING: Active searchPillView not found on root folder.");
-            }
-        }
-    }
-}
+
