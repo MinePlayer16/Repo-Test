b19ab2edfa67010122d0f7711ccbc49e45e55bcf
Iggy05 <PixelBurnYTMail@gmail.com>
2026-06-16 18:09:45 +0200

    Update diagnostic.js with dockAlpha parameter
    Added parameter documentation for dockAlpha transparency.

--- tweaks/diagnostic.js
+++ tweaks/diagnostic.js
@@ -1,4 +1,5 @@
 // @param: switch | enableBlinker | Enable Test Blinker | true
+// @param: slider | dockAlpha | Trasparenza Dock | 0.5 | 0.0-1.0
 
 log("Test Blinker: Script avviato, generazione sicura...");
 
